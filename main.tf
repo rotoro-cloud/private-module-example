8a924accfa33432d0b352332a8477ae2c4d520fd
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2021-09-29 10:58:27 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.region
+}
+
+module "s3-webapp" {
+  source  = "app.terraform.io/rotoro-corp/s3-webapp/aws"
+
+  name        = var.name
+  region = var.region
+  prefix = var.prefix
+  version = "1.0.0"
+}

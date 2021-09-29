c42e8e6b57e747f8d593afb04faabcc060be42cb
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2021-09-29 12:32:45 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/rotoro-corp/s3-webapp/aws"
+  source  = "app.terraform.io/rotoro-demo/s3-webapp/aws"
 
   name        = var.name
   region = var.region
